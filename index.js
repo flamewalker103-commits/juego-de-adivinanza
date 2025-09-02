f9297ac7bcea84a1c57e4fa1276a0918214181e3
flamewalker103-commits <ouchalesho@hotmail.com>
2025-09-02 11:17:45 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+class JuegoDeAdivinanza {
+    constructor(palabraSecreta) {
+        this.palabraSecreta = palabraSecreta;
+        this.intentos = 5;
+        this.pista = '';
+    }
+
+    establecerPista(pista) {
+        this.pista = pista;
+    }
+
+    jugar() {
+        console.log('Bienvenido al Juego de Adivinanza!');
+        console.log(`Tienes ${this.intentos} intentos para adivinar la palabra.`);
+        console.log(`Pista: ${this.pista}`);
+
+        while (this.intentos > 0) {
+            let adivinanza = prompt('Introduce tu adivinanza: ');
+            if (adivinanza === this.palabraSecreta) {
+                console.log('¡Felicidades! Adivinaste la palabra.');
+                return;
+            } else {
+                this.intentos--;
+                console.log(`Incorrecto. Te quedan ${this.intentos} intentos.`);
+            }
+        }
+        console.log(`Juego terminado. La palabra era: ${this.palabraSecreta}`);
+    }
+}
+
+const juego = new JuegoDeAdivinanza('javascript');
+juego.establecerPista('Es un lenguaje de programación ampliamente utilizado.');
+juego.jugar();
